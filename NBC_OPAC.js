38a1eba762630d4aefaf6e1edb03c6ccbc8ce80d
orangkucing <info@mewpro.cc>
2024-03-11 05:56:57 +0900

    Update NBC_OPAC.js

--- NBC_OPAC.js
+++ NBC_OPAC.js
@@ -141,7 +141,7 @@ async function sendQuery(event) {
 
     const script = document.createElement('script');
     script.src = 'https://docs.google.com/spreadsheets/d/1-XgySBso-vJoqMhmYgUZMtjcCY0qnjm-vIr3c6J7_M8/gviz/tq?tqx=out:json;responseHandler:callback&headers=1&sheet=%22' + sheetname + '%22&tq=' + encodeURIComponent(q);
-    document.body.lastElementChild.replaceWith(script);
+    document.head.appendChild(script);
     // callback function dataHandler() is called.
   } else {
     document.getElementById('results').innerHTML = '';
