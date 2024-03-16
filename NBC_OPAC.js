ac12da4479ef2182fb5126d786b9234812bc7bdf
orangkucing <info@mewpro.cc>
2024-03-17 08:03:51 +0900

    Update NBC_OPAC.js

--- NBC_OPAC.js
+++ NBC_OPAC.js
@@ -1,5 +1,5 @@
 // change here! if column or sheet names are modified in the sheet
-const selectA1 = {'著者・編者':'B', '読み':'C', 'シリーズ名':'D', '巻号':'E', 'タイトル':'F', '出版者':'G', '出版年':'H', '分類記号':'I', '配架場所':'K', 'NDL書誌ID':'J', '出版者備考':'N'};
+const selectA1 = {'著者・編者':'B', '読み':'C', 'シリーズ名':'D', '巻号':'E', 'タイトル':'F', '出版者':'H', '出版年':'J', '分類記号':'K', '配架場所':'M', 'NDL書誌ID':'L', '出版者備考':'I'};
 const yomiIndex = 1;
 const authorIndices = [0, yomiIndex];
 const titleIndices = [2, 4];
